10f2f5ac493b3e59e41f08a8b004daf1b39d8429
ferdie bergado <ferdiebergado@gmail.com>
2025-07-20 19:29:33 +0800

    refactor: Properly layout the auth pages

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,12 @@
+<div class="flex-wrapper">
+	<slot />
+</div>
+
+<style>
+	.flex-wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+</style>
