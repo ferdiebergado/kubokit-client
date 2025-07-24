78135748061d7352c14619181fcddcc33310fcb7
ferdie bergado <ferdiebergado@gmail.com>
2025-07-24 10:48:48 +0800

    refactor: Log a message

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+console.log('Loading modules...');
+
 export const api = 'http://localhost:8888' as const;
 
 export type APIResponse<T, E> = {
