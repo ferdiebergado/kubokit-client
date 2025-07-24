83cfa7ab1b8f89d6540e44288676350e9bbe0b88
ferdie bergado <ferdiebergado@gmail.com>
2025-07-24 21:45:35 +0800

    refactor: Add other routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,5 +1,8 @@
 export const routes = Object.freeze({
+	register: '/auth/register',
 	login: '/auth/login',
 	refresh: '/auth/refresh',
-	verify: '/auth/verify'
+	verify: '/auth/verify',
+	logout: '/auth/logout',
+	users: '/users'
 } as const);
