08de7c10d35d4e6f15a28e592da2d76958d15f23
ferdie bergado <ferdiebergado@gmail.com>
2025-07-24 17:58:08 +0800

    feat: Add json fetch wrapper

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
+import { originalFetch } from '../routes/state.svelte';
+
 // place files you want to import through the `$lib` alias in this folder.
 console.log('Loading modules...');
 
@@ -6,5 +8,24 @@ export const api = 'http://localhost:8888' as const;
 export type APIResponse<T, E> = {
 	message: string;
 	data?: T;
-	errors?: E;
+	errors?: E & { error_code?: string };
 };
+
+export async function jsonFetch<T extends object>(
+	url: RequestInfo | URL,
+	opts?: Omit<RequestInit, 'body'>,
+	body?: T
+): Promise<Response> {
+	const optsWithOptionalBody: RequestInit = {
+		...opts
+	};
+
+	if (body) {
+		optsWithOptionalBody.body = JSON.stringify(body);
+	}
+	const req = new Request(url, optsWithOptionalBody);
+	const headers = new Headers(req.headers);
+	headers.set('Content-Type', 'application/json');
+	const res = await originalFetch(req, { headers });
+	return res;
+}
