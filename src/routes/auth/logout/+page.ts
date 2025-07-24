8bfc25b5fdb52d92b3bdc335fc773d20d757b9e3
ferdie bergado <ferdiebergado@gmail.com>
2025-07-24 19:07:19 +0800

    refactor: Redirect to login page on logout

--- src/routes/auth/logout/+page.ts
+++ src/routes/auth/logout/+page.ts
@@ -1,8 +1,9 @@
+import { routes } from '$lib/routes';
 import { authClient } from '../../state.svelte';
 import type { PageLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load = (async () => {
 	authClient.clearSession();
-	throw redirect(303, '/');
+	throw redirect(303, routes.login);
 }) satisfies PageLoad;
