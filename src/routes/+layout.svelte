e125cade23c3939edeb82ee6aad5e2020287c921
ferdie bergado <ferdiebergado@gmail.com>
2025-07-25 17:47:10 +0800

    refactor: Move alert on the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import '../styles/global.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import { appState } from './state.svelte';
+	import { Alert } from '$lib/components';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+	let msg = $derived(appState.msg);
+	let cls = $derived(appState.success ? 'success' : 'error');
 </script>
 
 <Navbar />
 
-<slot />
+{#if msg}
+	<Alert message={msg} {cls}></Alert>
+{/if}
+
+{@render children()}
