1cb55e693206318fcf4029523bcb761714e7db6b
ferdie bergado <ferdiebergado@gmail.com>
2025-07-25 17:50:21 +0800

    feat: Implement email verification

--- src/routes/auth/verify/+page.ts
+++ src/routes/auth/verify/+page.ts
@@ -1,19 +1,27 @@
-import { api, jsonFetch, type APIResponse } from '$lib';
+import { api, type APIResponse } from '$lib';
 import { routes } from '$lib/routes';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { appState } from '../../state.svelte';
 
 export const load = (async ({ url }) => {
-	const res = await jsonFetch(
-		api + routes.verify,
-		{ method: 'POST' },
-		{ token: url.searchParams.get('token') }
-	);
-
-	if (!res.ok) {
-		const { message }: APIResponse<undefined, undefined> = await res.json();
-		throw new Error(message);
+	const token = url.searchParams.get('token');
+	if (!token) {
+		appState.msg = 'Invalid verification link.';
+		appState.success = false;
+		throw redirect(303, routes.login);
 	}
 
+	const body = { token };
+	const res = await api.post(routes.verify, false, body);
+	const { message }: APIResponse<undefined, undefined> = await res.json();
+	appState.msg = message;
+
+	if (!res.ok) {
+		appState.success = false;
+		throw redirect(303, routes.login);
+	}
+
+	appState.success = true;
 	throw redirect(303, routes.login);
 }) satisfies PageLoad;
