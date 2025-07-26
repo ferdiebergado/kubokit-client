3a8fac5b77d31d44deb4f043cfa6084bbef2ffd2
ferdie bergado <ferdiebergado@gmail.com>
2025-07-26 15:23:05 +0800

    feat: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div>
+	<h1>{page.status}: {page.error?.message}</h1>
+</div>
+
+<style>
+	h1 {
+		color: lightgrey;
+		font-size: 3.2rem;
+	}
+
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: calc(100vh - 254px);
+	}
+</style>
