022041abda719de260cd38da2243b50fa22f860a
ferdie bergado <ferdiebergado@gmail.com>
2025-07-26 14:46:34 +0800

    refactor: Handle not ok response

--- src/routes/(protected)/users/+page.ts
+++ src/routes/(protected)/users/+page.ts
@@ -1,21 +1,29 @@
 import { api, type APIResponse } from '$lib/api';
 import type { Users } from '$lib/features/users';
 import { routes } from '$lib/routes';
+import { error } from '@sveltejs/kit';
+import { appState } from '../../state.svelte';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ parent }) => {
 	await parent();
 
 	const res = await api.get(routes.users, true);
-	const { data }: APIResponse<Users, undefined> = await res.json();
 
-	if (data) {
-		return {
-			users: data.users
-		};
+	if (!res.ok) {
+		const { message }: APIResponse<undefined, undefined> = await res.json();
+
+		appState.msg = message;
+		appState.success = false;
+
+		error(res.status, {
+			message
+		});
 	}
 
+	const { data }: APIResponse<Users, undefined> = await res.json();
+
 	return {
-		users: []
+		users: data!.users
 	};
 }) satisfies PageLoad;
